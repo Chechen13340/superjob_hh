c1046e722be8ccdaeddc10295d68483321e8d781
Artem Osipov <tema.osipov@mail.ru>
2023-08-05 18:17:41 +0300

    created abstract class and abstrasct method for parser class

---
+++ src/abstract_class.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod, ABC
+
+
+class HH_SJ_API(ABC):
+    def __init__(self, url: str, headers: dict, params: dict):
+        self.url = url
+        self.headers = headers
+        self.params = params
+
+    @abstractmethod
+    def get_data(self) -> dict:
+        pass
