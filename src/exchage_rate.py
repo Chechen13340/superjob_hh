11fcecc2f5979b9468e11447ec8271a8dca1d468
Artem Osipov <tema.osipov@mail.ru>
2023-08-06 20:31:19 +0300

    created special method for change currency

--- src/exchage_rate.py
+++ src/exchage_rate.py
@@ -3,7 +3,7 @@ import requests
 from superjob_hh.settings import API_KEY, ER_API_URL
 
 
-def get_currency_rate(base: str) -> float:
+def get_currency_rate(base='USD') -> float:
     """Получает курс доллара по API и вовзращает float"""
 
     response = requests.get(ER_API_URL, headers={'apikey': API_KEY}, params={'base': base})
