d9832713474ad2c3f6b64ac9f062378781605fa3
Artem Osipov <tema.osipov@mail.ru>
2023-08-06 11:30:23 +0300

    created class for save json file for user

--- src/HH_parser.py
+++ src/HH_parser.py
@@ -8,7 +8,6 @@ from superjob_hh.src.abstract_class import HH_SJ_API
 
 class HeadHunterParser(HH_SJ_API):
     def __init__(self, vacansy_name: str, region: int, page_number: int, count_per_page: int):
-
         super().__init__(url=HH_API_URL, headers={"User-Agent": "HH-User-Agent"}, params={
             'text': vacansy_name,
             'area': region,
@@ -20,6 +19,11 @@ class HeadHunterParser(HH_SJ_API):
         response = requests.get(self.url, headers=self.headers, params=self.params)
         data = response.json()
         return data['items']
+
+
+class JSONSaver(HeadHunterParser):
+    def __init__(self, vacansy_name: str, region: int, page_number: int, count_per_page: int):
+        super().__init__(vacansy_name, region, page_number, count_per_page)
 
     def data_dict(self) -> dict:
         vacancy = []
