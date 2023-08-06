0b0cd24745853abc73d9bbed09d27be5289db839
Artem Osipov <tema.osipov@mail.ru>
2023-08-06 20:29:11 +0300

    add settings variables

--- settings.py
+++ settings.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import os
+
+ROOT_PATH = Path(__file__).parent
+OPEARTION_PATH = Path.joinpath(ROOT_PATH, 'vacancy.csv')
+
 HH_API_URL = 'https://api.hh.ru/vacancies'
 
-
+ER_API_URL = "https://api.apilayer.com/exchangerates_data/latest"
+API_KEY = os.getenv('API_KEY_Ex_Rate')
