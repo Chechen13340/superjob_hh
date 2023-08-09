b997c332f7e97303575d05e08c751395594416c8
Artem Osipov <tema.osipov@mail.ru>
2023-08-09 21:33:45 +0300

    fixed functions for work with user from console

--- src/functions.py
+++ src/functions.py
@@ -4,6 +4,7 @@ from superjob_hh.src.HH_parser import Vacancies, JSONSaver
 def user_get_data():
     """
     Функция для получения информации по API
+    и сортировки данных по заработной плате
     возвращает экземпляр класса JSONSaver
     """
     user_vacansy = input('Введите название вакансии ')
@@ -11,19 +12,19 @@ def user_get_data():
     user_page = int(input('Введите номер страницы поиска  (начинается с нулевой) '))
     user_count_per_page = int(input('Количество объявлений на странице '))
 
-    hh = Vacancies(user_vacansy, user_area, user_page, user_count_per_page)
-    hh.validation()
-    json_saver = JSONSaver(user_vacansy, user_area, user_page, user_count_per_page)
-    json_saver.save_json(hh.validation())
+    request_salary = input(
+        'Отсортировать список вакансий по средней заработной плате ("По убыванию"/"По возрастанию")? ')
+    hh = Vacancies(user_vacansy, user_area, user_page, user_count_per_page, request_salary)
+    hh.sort_data_of_salary()
+    json_saver = JSONSaver(user_vacansy, user_area, user_page, user_count_per_page, request_salary)
+    json_saver.save_json(hh.sort_data_of_salary())
     return json_saver
 
 
 def user_sort():
     """
     Функция для сортировки списка вакансий по ключу словаря.
-    Выводит отсортированную информацию по ключу словаря.
-    Ничего не возвращает
-    :return:
+    Выводит отсортированную по зарплате информацию по ключу словаря.
     """
     json_saver = user_get_data()
     user_request = input('Введите информацию для сортировки:(job_title, location, employer, url, requirement, '
@@ -43,10 +44,9 @@ def user_sort():
 
 def del_info():
     """
-    Функция для удаления информации по
-    ключу из словаря. Возвращает отсортированные
-    , в том числе после удаления,
-    по желанию пользователя данные в txt файл.
+    Функция для удаления информации по ключу
+     из словаря. Возвращает отсортированные,
+     в том числе после удаления, данные в txt файл.
     """
     json_saver = user_sort()
     user_key = input('Введите ключевое слово чтобы удалить ненужные данные (для завершения введите "Выход" ): ')
