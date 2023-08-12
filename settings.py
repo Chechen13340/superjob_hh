6adf9510a3477cfccd8dcb0b474d9cafe939de28
Artem Osipov <tema.osipov@mail.ru>
2023-08-12 21:08:56 +0300

    added path for json HH and json SJ

--- settings.py
+++ settings.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import os
 
 ROOT_PATH = Path(__file__).parent
-OPEARTION_PATH = Path.joinpath(ROOT_PATH, 'vacancy.csv')
-
+OPEARTION_PATH_HH = Path.joinpath(ROOT_PATH, 'vacancy.csv')
+OPEARTION_PATH_SJ = Path.joinpath(ROOT_PATH, 'vacancy_sj.csv')
+SJ_API_URL = 'https://api.superjob.ru/2.0/vacancies/'
 HH_API_URL = 'https://api.hh.ru/vacancies'
 
 ER_API_URL = "https://api.apilayer.com/exchangerates_data/latest"
